384086c2ea86e97819ff2b417a7b587dd4c4bfda
amshehaly <amshehaly@gmail.com>
2023-12-17 14:22:47 +0200

    adding apple-splash configs

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -13,6 +13,18 @@ import {
     preset: combinePresetAndAppleSplashScreens(minimal2023Preset, {
       // dark splash screens using black background (the default)
       darkResizeOptions: { background: 'black', fit: 'contain' },
+      linkMediaOptions:{
+        addMediaScreen: true,
+        log: true,
+        xhtml: false
+      },
+      png: {
+        compressionLevel: 9,
+        quality: 60
+      },
+      name: (landscape, size, dark) => {
+        return `apple-splash-${landscape ? 'landscape' : 'portrait'}-${typeof dark === 'boolean' ? (dark ? 'dark-' : 'light-') : ''}${size.width}x${size.height}.png`
+      }
       // or using a custom background color
       // darkResizeOptions: { background: '#1f1f1f' },
     }, [... AllAppleDeviceNames]),
