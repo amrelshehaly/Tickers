9b9c9e30cc30703e9f3bcb517af56d88aee3ec05
amshehaly <amshehaly@gmail.com>
2023-12-17 13:01:09 +0200

    documenting the useTicker hook

--- src/hooks/useTicker.ts
+++ src/hooks/useTicker.ts
@@ -3,17 +3,6 @@ import { getTickers } from "../api/tickers";
 import { useStore } from "../context/store";
 import { ErrorProps } from "../api/types";
 
-// type tickeHookProps = {
-
-// }
-
-/**
- * 
- * @returns This Hook return the Response from the getTicker API, loading state and if errorFound. 
- *          Also you can call fetchMore to load more data.
- * @
- */
-
 const useTicker = () => {
 
     const [ error, setError ] = useState<ErrorProps| undefined>(undefined)
@@ -23,6 +12,9 @@ const useTicker = () => {
 
   const url = response.next_url || "https://api.polygon.io/v3/reference/tickers?active=true&limit=20";
 
+  /**
+   * @function  This function fetches new tickers and concat it with the previous ticlers already loaded
+   */
   const fetchData = async () => {
     setLoading(true)
     try {
@@ -41,20 +33,30 @@ const useTicker = () => {
         setLoading(false)
     } catch (error) {
         console.log("ERROR", error);
+        setLoading(false)
     }
   };
 
+  /**
+   *  @function This function is fired when screen reached scrollthreshold
+   */
   const fetchMore = () => {
     if (response.status === "OK") {
       fetchData();
     }
   };
 
+  /**
+   *  @function This function help user to refetch the tickers if an error occurs
+   */
   const reFetchData = () => {
     setError(undefined)
     fetchData()
   }
 
+    /**
+   *  @function  This function clears any stored tickers or response from the already fteched tickers
+   */
   const restResult =  () => {
     clearResults()
   }
